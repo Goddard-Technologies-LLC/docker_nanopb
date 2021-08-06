37930a75a559674ddb8ac1f20a7ef9639f4c1e80
Yvan Pearson <yvan.pearson@gmail.com>
2021-08-06 10:39:57 -0400

    oops

--- generate_nanopb.sh
+++ generate_nanopb.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cp -v nanopb-${NANOPB_VER}/pb*.? host_mnt/
-nanopb-${NANOPB_VER}/generator/protoc --nanopb_out=. host_mnt/*.proto > host_mnt/nanopb_out.log 2>&1
-nanopb-${NANOPB_VER}/generator/protoc --python_out=. host_mnt/*.proto > host_mnt/python_out.log 2>&1
+cd host_mnt
+../nanopb-${NANOPB_VER}/generator/protoc --nanopb_out=. *.proto > nanopb_out.log 2>&1
+../nanopb-${NANOPB_VER}/generator/protoc --python_out=. *.proto > python_out.log 2>&1
